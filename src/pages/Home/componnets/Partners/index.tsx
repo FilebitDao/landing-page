44c094c06a6c79c446e8c3084a7ad947f863ec8b
rwson <rwson92@gmail.com>
2023-03-09 19:52:31 +0800

    Feat: some changes and add ignore file

--- src/pages/Home/componnets/Partners/index.tsx
+++ src/pages/Home/componnets/Partners/index.tsx
@@ -1,9 +1,6 @@
 import React, { FC, useMemo } from 'react';
 import { useBreakpoint } from 'use-breakpoint';
 
-import { ReactComponent as ProtolLabs } from 'assets/imgs/protol.svg';
-import { ReactComponent as BinanceLabs } from 'assets/imgs/binance-labs.svg';
-import { ReactComponent as BIBExchange } from 'assets/imgs/bib.svg';
 import { ReactComponent as IPFS } from 'assets/imgs/ipfs.svg';
 
 import styles from './index.module.scss';
@@ -14,35 +11,11 @@ const Partners: FC = () => {
   const { breakpoint } = useBreakpoint(breakpoints);
   const size = useMemo(() => {
     return breakpoint !== 'desktop' ? {
-      protolLabs: {
-        width: 36,
-        height: 35
-      },
-      binanceLabs: {
-        width: 142,
-        height: 37
-      },
-      bibExchange: {
-        width: 78,
-        height: 32
-      },
       ipfs: {
         width: 93,
         height: 40
       }
     } : {
-      protolLabs: {
-        width: 56,
-        height: 55
-      },
-      binanceLabs: {
-        width: 202,
-        height: 53
-      },
-      bibExchange: {
-        width: 128,
-        height: 52
-      },
       ipfs: {
         width: 153,
         height: 66
@@ -56,21 +29,21 @@ const Partners: FC = () => {
       <div className={styles.partnerContainer}>
         <div className={styles.partner}>
           <div className={styles.partnerLogo}>
-            <ProtolLabs {...size.protolLabs} />
+            <IPFS {...size.ipfs} />
           </div>
-          <p className={styles.partnerName}>Protocol Labs</p>
+          <p className={styles.partnerName}>IPFS</p>
         </div>
         <div className={styles.partner}>
           <div className={styles.partnerLogo}>
-            <BinanceLabs {...size.binanceLabs} />
+            <IPFS {...size.ipfs} />
           </div>
-          <p className={styles.partnerName}>Binance Labs</p>
+          <p className={styles.partnerName}>IPFS</p>
         </div>
         <div className={styles.partner}>
           <div className={styles.partnerLogo}>
-            <BIBExchange {...size.bibExchange} />
+            <IPFS {...size.ipfs} />
           </div>
-          <p className={styles.partnerName}>BIB Exchange</p>
+          <p className={styles.partnerName}>IPFS</p>
         </div>
         <div className={styles.partner}>
           <div className={styles.partnerLogo}>
